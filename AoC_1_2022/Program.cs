30bb93b70519b2d05e5014b988a873aea7f4aa79
LFY <filip.zdjelar@gmail.com>
2022-12-01 14:53:56 +0100

    Add project files.

---
+++ AoC_1_2022/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> data = File.ReadLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt")).ToList();
+List<int> sums = new();
+
+int calories;
+int sum = 0;
+
+foreach (string line in data)
+{
+    if (int.TryParse(line, out calories))
+    {
+        sum += calories;
+    }
+    else
+    {
+        sums.Add(sum);
+        sum = 0;
+    }
+}
+
+Console.WriteLine($"resultEasy: {sums.Max()}");
+Console.WriteLine($"resultHard: {sums.OrderByDescending(x => x).Take(3).Sum()}");
