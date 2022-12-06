c235b0361316acd81ec55f5161247e1da51e1dff
LFY <filip.zdjelar@gmail.com>
2022-12-06 20:48:05 +0100

    Added day 6 solution

---
+++ AoC_6_2022/Program.cs
@@ -0,0 +1,7 @@
+﻿string sample = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+
+string SolveEasy(string input) => $"{Enumerable.Range(4, input.Length).First(i => input[(i - 4)..i].Distinct().Count() == 4)}";
+string SolveHard(string input) => $"{Enumerable.Range(14, input.Length).First(i => input[(i - 14)..i].Distinct().Count() == 14)}";
+
+Console.WriteLine("easy:" + SolveEasy(sample));
+Console.WriteLine("hard:" + SolveHard(sample));
