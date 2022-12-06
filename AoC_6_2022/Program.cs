f61b6f9c0ae3a7521e2a477406b08b6fa2077475
LFY <filip.zdjelar@gmail.com>
2022-12-06 20:50:25 +0100

    :)

--- AoC_6_2022/Program.cs
+++ AoC_6_2022/Program.cs
@@ -1,7 +1,4 @@
-﻿string sample = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+﻿string gameData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
 
-string SolveEasy(string input) => $"{Enumerable.Range(4, input.Length).First(i => input[(i - 4)..i].Distinct().Count() == 4)}";
-string SolveHard(string input) => $"{Enumerable.Range(14, input.Length).First(i => input[(i - 14)..i].Distinct().Count() == 14)}";
-
-Console.WriteLine("easy:" + SolveEasy(sample));
-Console.WriteLine("hard:" + SolveHard(sample));
+Console.WriteLine("easy:" + $"{Enumerable.Range(4, gameData.Length).First(i => gameData[(i - 4)..i].Distinct().Count() == 4)}");
+Console.WriteLine("easy:" + $"{Enumerable.Range(14, gameData.Length).First(i => gameData[(i - 14)..i].Distinct().Count() == 14)}");
