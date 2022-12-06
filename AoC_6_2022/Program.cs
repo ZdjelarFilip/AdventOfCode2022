041e7b70f141e64736387a10fee8a5587ed377d7
LFY <filip.zdjelar@gmail.com>
2022-12-06 20:55:06 +0100

    updated easy -> hard description

--- AoC_6_2022/Program.cs
+++ AoC_6_2022/Program.cs
@@ -1,4 +1,4 @@
 ﻿string gameData = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
 
 Console.WriteLine("easy:" + $"{Enumerable.Range(4, gameData.Length).First(i => gameData[(i - 4)..i].Distinct().Count() == 4)}");
-Console.WriteLine("easy:" + $"{Enumerable.Range(14, gameData.Length).First(i => gameData[(i - 14)..i].Distinct().Count() == 14)}");
+Console.WriteLine("hard:" + $"{Enumerable.Range(14, gameData.Length).First(i => gameData[(i - 14)..i].Distinct().Count() == 14)}");
