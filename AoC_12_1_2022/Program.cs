b2b560ed56911c32d5c32d87d68fbda2d3af130e
LFY <filip.zdjelar@gmail.com>
2022-12-13 00:33:18 +0100

    Day 12

---
+++ AoC_12_1_2022/Program.cs
@@ -0,0 +1,9 @@
+﻿static class Program
+{
+    static void Main()
+    {
+        var gameData = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+        Console.WriteLine($"easy: {HillClimb.ProcessEasy(gameData)}");
+        Console.WriteLine($"hard: {HillClimb.ProcessHard(gameData)}");
+    }
+}
